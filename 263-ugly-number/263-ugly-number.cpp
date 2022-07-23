76c4251fe12217a49c9b85f7cfa67d4a7418e775
Akshay Kumar Singh <75743797+Akshay-kumar01@users.noreply.github.com>
2022-07-24 00:28:50 +0530

    Time: 5 ms (24.72%), Space: 5.9 MB (20.33%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -3,17 +3,16 @@ public:
     bool isUgly(int n) {
         if(n==0)
             return false;
-        while(n%2==0&&n!=0)
+        while(n!=0)
         {
-            n=n/2;
-        }
-        while(n%3==0&&n!=0)
-        {
-            n=n/3;
-        }
-        while(n%5==0&&n!=0)
-        {
-            n=n/5;
+            if(n%2==0)
+                n/=2;
+            else if(n%3==0)
+                n/=3;
+            else if(n%5==0)
+                n/=5;
+            else
+                break;
         }
         if(n!=0&&n!=1)
             return false;
